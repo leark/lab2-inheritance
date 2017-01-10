175eeae6aa7ba5deff04f3d251348cb6c39c9320
Seung Lee <shl7@uw.edu>
2017-01-10 10:38:09 -0800

    completed part1

---
+++ lab2-part1.ts
@@ -0,0 +1,31 @@
+class bankAccount {
+    private balance:number;
+    private ownerName:string;
+
+    constructor(initialBalance:number, ownerName:string) {
+        this.balance = initialBalance;
+        this.ownerName = ownerName;
+    }
+
+    deposit(amount:number):void {
+        this.balance += amount;
+    }
+
+    withdraw(amount:number):void {
+        this.balance -= amount;
+    }
+
+    checkBalance():void {
+        console.log(`Balance: ${this.balance} Owner: ${this.ownerName}`);
+    }
+}
+
+let myBank:bankAccount = new bankAccount(322, "Mine");
+console.log("Making account with 322 and owner Mine");
+myBank.checkBalance();
+myBank.deposit(78);
+console.log("Adding 78");
+myBank.checkBalance();
+console.log("Subtracting 78");
+myBank.withdraw(78);
+myBank.checkBalance();
